bc3a440ec59c7aa08ab26ab8e4521b5dbb8b91cd
zhidev <Doug@douglass-macbook-air.local>
2016-03-31 02:39:50 -0700

    First update. Content inside view constantly resetting on tap

---
+++ OverlayedViewExp/CustomView.swift
@@ -0,0 +1,88 @@
+//
+//  CustomView.swift
+//  OverlayedViewExp
+//
+//  Created by Douglas on 3/30/16.
+//  Copyright © 2016 Dougli. All rights reserved.
+//
+
+import UIKit
+
+class CustomView: UIView {
+
+    
+    @IBOutlet var contentView: UIView!
+    
+    @IBOutlet var imageContent: UIImageView!
+    
+    @IBOutlet var imagePan: UIPanGestureRecognizer!
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        initSubviews()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initSubviews()
+    }
+    
+    func initSubviews() {
+        let nib = UINib(nibName: "CustomView", bundle: nil)
+        nib.instantiateWithOwner(self, options: nil)
+        contentView.frame = bounds
+        imageContent.contentMode = UIViewContentMode.ScaleAspectFill
+        contentView.backgroundColor = UIColor.grayColor()
+        imageContent.clipsToBounds = true
+        addSubview(contentView)
+    }
+    
+    
+    @IBAction func onImagePan(panGestureRecognizer: UIPanGestureRecognizer) {
+        var point = panGestureRecognizer.locationInView(self.contentView)
+        if panGestureRecognizer.state == UIGestureRecognizerState.Began {
+            print("Began")
+        } else if panGestureRecognizer.state == UIGestureRecognizerState.Changed {
+            
+            //--------------------------------------------------------
+            // Make sure we stay within the bounds of the parent view
+            //--------------------------------------------------------
+            //Taken from http://stackoverflow.com/questions/9314045/dragging-a-specific-uiimageview and translated to swift and this portion
+            
+            let midPointX = CGRectGetMidX(self.imageContent.bounds)
+            // If too far right...
+            if (point.x > self.imageContent.superview!.bounds.size.width  - midPointX){
+                point.x = self.imageContent.superview!.bounds.size.width - midPointX
+            }
+            else if (point.x < midPointX){  // If too far left...
+                point.x = midPointX
+            }
+            
+            let midPointY = CGRectGetMidY(self.imageContent.bounds)
+            // If too far down...
+            if (point.y > self.imageContent.superview!.bounds.size.height  - midPointY){
+                point.y = self.imageContent.superview!.bounds.size.height - midPointY
+            }
+            else if (point.y < midPointY){  // If too far up...
+                point.y = midPointY
+            }
+            
+            // Set new center location
+            //self.center = newPoint Moved to animate with duration
+            
+            
+            UIView.animateWithDuration(0.25, animations: { () -> Void in
+                self.imageContent.center = point
+                print("Changed")
+            })
+        }else if panGestureRecognizer.state == UIGestureRecognizerState.Ended{
+            print("Ended")
+        }
+
+
+    }
+    
+    
+}
+
+
--- OverlayedViewExp/ViewController.swift
+++ OverlayedViewExp/ViewController.swift
@@ -10,9 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    
+    var customViewWhenOpen: CGRect!
+    var customViewWhenClosed: CGRect!
+    var trueForOpenFalseForClosed = false
+    
+    
+    var testView: CustomView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        print(" VIEW : \(self.view.frame)")
+        let value = UIScreen.mainScreen().bounds
+        print(value)
+        
+        customViewWhenOpen = CGRectMake(0, 0, self.view.frame.size.width/2.5, self.view.frame.size.height)
+        customViewWhenClosed = CGRectMake(-self.view.frame.size.width/2.5 + 15, 0, self.view.frame.size.width/2.5, self.view.frame.size.height)
+        
+        testView = CustomView(frame: customViewWhenClosed)
+        testView.imageContent.image = UIImage(named: "gradient")
+        testView.userInteractionEnabled = true
+
+        view.addSubview(testView)
+        trueForOpenFalseForClosed = false
+
+        
+        print(testView.frame)
+        let TapGesture = UITapGestureRecognizer(target: self, action: "tappedCustomView:")
+        testView.addGestureRecognizer(TapGesture)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +49,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+
+    func tappedCustomView(sender: UITapGestureRecognizer) {
+        print(" TAP TAP REV")
+        UIView.animateWithDuration(2) { () -> Void in
+            //self.testView.center = CGPoint(x: self.testView.center.x + 200, y: self.testView.center.y+300)
+            if( self.trueForOpenFalseForClosed){
+                self.testView.frame = self.customViewWhenClosed
+                self.trueForOpenFalseForClosed = false
+            }else{
+                self.testView.frame = self.customViewWhenOpen
+                self.trueForOpenFalseForClosed = true
+            }
+        }
+    }
 
 }
 
